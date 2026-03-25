e37968fe7d69182820f0fdf537a7e93542972af2
floam <kasinovviktorr@mail.ru>
2026-03-25 17:20:23 +0300

    done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    let result = "";
 
+    if (count < 0) {
+        return 'ERROR';
+    }
+
+    while (count--) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str  = str.toLowerCase();
+    let result = str.split('').filter(
+        function(item) {
+            return ('a' <= item && item <= 'z') || ('0' <= item && item <= '9');
+        }
+    );
+  
+    let copy_rs = result.slice();
+    result.reverse()
+    return copy_rs.join('') == result.join('')
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
